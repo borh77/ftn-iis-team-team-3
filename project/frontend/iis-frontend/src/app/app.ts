65eb9ece48c2468b5e80879fd68bdb106436f87c
borh77 <borisahrnjez@gmail.com>
2026-05-25 22:01:16 +0200

    feat: design now matches protoype

--- project/frontend/iis-frontend/src/app/app.ts
+++ project/frontend/iis-frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
